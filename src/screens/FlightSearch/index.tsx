9a9db3788355c8a9bd48980bea9ecc93335312a8
ishabhattt <bhattisha000@gmail.com>
2024-01-23 12:42:56 +0530

    Temp

--- src/screens/FlightSearch/index.tsx
+++ src/screens/FlightSearch/index.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { SafeScreen } from "@/components/template";
-import FlightSearchCard from "@/components/template/FlightSearchCard";
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeScreen } from '@/components/template';
+import FlightSearchCard from '@/components/template/FlightSearchCard';
 
 const FlightSearch = () => {
+  console.log('temp');
   return (
     <SafeScreen>
       <FlightSearchCard />
